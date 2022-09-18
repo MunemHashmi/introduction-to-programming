e8b526c7d67c0006594290f7121989715d15637b
Munem Hashmi <88432708+MunemHashmi@users.noreply.github.com>
2022-09-18 05:35:44 +0500

    Add files via upload

---
+++ Lectures/Data Types and Operators/data_types.cpp
@@ -0,0 +1,20 @@
+// This is an illustration of the use of fundamental data types in C++.
+#include <iostream>  
+using namespace std; 
+int main() 
+{ 
+    // Fundamental data types in C++ 
+    int i = 10; 
+    char c = 'A'; 
+    float f = 1.5; 
+    double d = 1.123456789; 
+    bool b = true; 
+  
+    // Displaying the values 
+    cout << "int: " << i << endl; 
+    cout << "char: " << c << endl; 
+    cout << "float: " << f << endl; 
+    cout << "double: " << d << endl; 
+    cout << "bool: " << b << endl; 
+    return 0; 
+} 
